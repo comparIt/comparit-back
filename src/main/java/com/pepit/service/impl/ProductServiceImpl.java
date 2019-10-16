61a2e57a9ed8eb6deac0a55a6c8dd8aba36a5bf2
Mitrail <edouardguegain@gmail.com>
2019-10-16 22:50:58 +0200

    WIP : Query API

--- src/main/java/com/pepit/service/impl/ProductServiceImpl.java
+++ src/main/java/com/pepit/service/impl/ProductServiceImpl.java
@@ -1,13 +1,12 @@
 package com.pepit.service.impl;
 
 import com.pepit.model.ProductDto;
+import com.pepit.util.Query;
 import com.pepit.repository.ProductRepositoryCustom;
-import com.pepit.repository.impl.ProductRepositoryCustomImpl;
 import com.pepit.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -18,43 +17,12 @@ public class ProductServiceImpl implements ProductService {
     ProductRepositoryCustom productRepositoryCustom;
 
     @Override
-    public List<ProductDto> search(Map<String, String> params) {
-        return productRepositoryCustom.testRequest(queryBuilder(params));
+    public List<ProductDto> search(Map<String, String> params, String order, Integer page) {
+        Query query = new Query();
+        query.addAllCriterias(params).page(page).addSorting(order);
+        return productRepositoryCustom.testRequest(query);
     }
 
-    private String queryBuilder(Map<String, String> params) {
-        StringBuilder sb = new StringBuilder();
 
-        params.entrySet().stream()
-                .map(e -> {
-                    switch (e.getKey()){
-                        case "a" : return intervalBuilder(e.getKey(), e.getValue());
-                        case "os" : return enumBuilder(e.getKey(), e.getValue());
-                        default: return "";
-                    }
-                })
-                .reduce((x,y) -> x + " AND " + y)
-                .map(sb::append);
-        System.out.println(sb.toString());
-        return sb.toString();
-    }
 
-    private String intervalBuilder(String k, String v) {
-        StringBuilder sb = new StringBuilder();
-        String[] interval = v.split("-");
-        int min = Integer.parseInt(interval[0]);
-        int max = Integer.parseInt(interval[1]);
-        sb.append(k).append(" >= ").append(min).append(" AND ").append(k).append(" <= ").append(max);
-        return sb.toString();
-    }
-
-    private String enumBuilder(String k, String v) {
-        StringBuilder sb = new StringBuilder();
-        String[] interval = v.split(",");
-        sb.append(k).append(" in ");
-        sb.append(" (");
-        Arrays.stream(interval).map(e -> "'"+e+"'").reduce((x,y) -> x + ", " + y).map(sb::append);
-        sb.append(")");
-        return sb.toString();
-    }
 }
