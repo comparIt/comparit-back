abad12319d5e766320dc7945e4d9d1c4f9720df2
Mitrail <edouardguegain@gmail.com>
2019-10-27 21:45:00 +0100

    WIP : clean code

--- src/main/java/com/pepit/controllers/CompanyController.java
+++ src/main/java/com/pepit/controllers/CompanyController.java
@@ -2,8 +2,6 @@ package com.pepit.controllers;
 
 import com.pepit.business.CompanyBusiness;
 import com.pepit.constants.Routes;
-import com.pepit.dto.ProductDto;
-import com.pepit.repository.ProductRepositoryCustom;
 import com.pepit.service.CompanyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -23,25 +21,12 @@ public class CompanyController {
         this.companyService = companyService;
     }
 
-    @PostMapping(value = "/")
-    public ResponseEntity<ProductDto> post(@RequestBody ProductDto product) {
-
-        if (product != null) {
-            System.out.println(product.getProperties());
-        }
-
-        // TODO: call persistence layer to update
-        return new ResponseEntity<ProductDto>(product, HttpStatus.OK);
-    }
-
-
-
     @CrossOrigin
     @GetMapping("/byUrl")
     @ResponseBody
     public ResponseEntity<String> sendUrlToGet(@RequestParam("url") String url, @RequestParam("supplierId") String supplierId, @RequestParam("type") String type){
         //TODO S'assurer que type reçu est coherent avec un des Model existant
         //TODO MAsquer le resultat qd c'est conforme result http only
-        return new ResponseEntity<String>(companyService.getFromUrl(url, supplierId, type), HttpStatus.OK);
+        return new ResponseEntity<>(companyService.getFromUrl(url, supplierId, type), HttpStatus.OK);
     }
 }
