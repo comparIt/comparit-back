3001ee4f96ba6a18992532b65c730009218e6d1f
Alexis <alexis.bels@outlook.fr>
2020-02-05 21:15:30 +0100

    WIP: endpoints en cours

--- src/main/java/com/pepit/controllers/UserController.java
+++ src/main/java/com/pepit/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.pepit.controllers;
 
 import com.pepit.constants.Routes;
 import com.pepit.dto.UserDto;
+import com.pepit.model.User;
 import com.pepit.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -24,9 +25,23 @@ public class UserController {
         return ResponseEntity.status(200).body(userService.create(userDto));
     }
 
+    @PutMapping("/saveSupplier")
+    public ResponseEntity<UserDto> createSupplier(@RequestBody UserDto userDto) {
+        return ResponseEntity.status(200).body(userService.createSupplier(userDto));
+    }
+
+    @PostMapping("/updateToSupplier")
+    public ResponseEntity<User> UpdateToSupplier(@RequestParam Integer userId) {
+        return ResponseEntity.status(200).body(userService.updateToSupplier(userId));
+    }
+
     @GetMapping("/currentUser")
     public ResponseEntity<UserDto> getCurrentUserByToken(){
         return ResponseEntity.status(200).body(userService.getUserDtoByToken());
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Integer userId) {
+        return ResponseEntity.status(200).body(userService.getUserById(userId));
+    }
 }
